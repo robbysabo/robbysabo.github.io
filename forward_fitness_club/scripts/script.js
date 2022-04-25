b4b5b16c51d1098218dd21cd04e6046bc453f32c
Robby <69889440+robbysabo@users.noreply.github.com>
2022-04-25 14:31:36 -0600

    Add files via upload

---
+++ forward_fitness_club/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Robert Sabo
+    File Name: script.js
+    Date: 4/25/2022
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function () {
+    $('.hero').height($(window).height());
+})
