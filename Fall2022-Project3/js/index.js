f8cb136bdf1631af7ea12cbab1362a876c518637
Robby <69889440+robbysabo@users.noreply.github.com>
2022-09-24 12:26:06 -0600

    Update index.js

--- Fall2022-Project3/js/index.js
+++ Fall2022-Project3/js/index.js
@@ -8,6 +8,7 @@
 *Modification log:
 
         Verison 1.0 - 09/09/2022 - Made Mod Log.
+        Verison 1.1 - 09/23/2022 - Created accordion for the FAQ. carousel effect of all the arcades inside the faq answer tab.
 
 ******************************************************************** */
 
